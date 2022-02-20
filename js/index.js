7093a03ca68f5adf519e22ef10d498d6f925f78e
Serhiy Pop <burlesque@MacBurlesque.local>
2022-02-20 14:34:34 +0300

    Added tasks 1-6

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//task2
+
+//alert("Pop");
+
+//task3
+let flower = "rose";
+let tree = "oak";
+console.log(flower)
+console.log(tree)
+flower = tree;
+console.log(flower)
+console.log(tree)
+
+//task4
+
+let myNewObj = {
+    "String": "5",
+    "Number": 5,
+    "Boolean": true,
+    "Undefined": undefined,
+    "Null": null 
+}
+
+//task5
+
+let isAdult = confirm("Are you an adult?");
+alert( isAdult );
+
+//task6
+
+const myFirstname = Serhiy;
+const myLastname = Pop;
+const myStudyGroup = "Lv-678.JS Core";
+const myBirthYear = 1987;
+
+let myMaritalStatus;
+myMaritalStatus = Married;
+
+console.log(typeof myBirthYear);
+
+console.log(typeof myFirstname);
+console.log(typeof myLastname);
+console.log(typeof myStudyGroup);
+
+console.log(typeof myMaritalStatus);
