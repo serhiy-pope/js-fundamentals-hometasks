22cb187f505fb0ad8a553d8a790a7c519ba8ae33
Serhiy Pop <sergey.pope@gmail.com>
2022-03-04 13:16:41 +0200

    Added second hometask (1-7)

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-//task2
+/*                              // First Hometask:
+                                        //task2
 
 alert("Pop");
 
-//task3
+                                        //task3
+
 let redFlower; 
 redFlower = "Poppy";
 let yellowFlower;
@@ -13,7 +15,7 @@ redFlower = yellowFlower;
 console.log(redFlower);
 console.log(yellowFlower);
 
-//task4
+                                        //task4
 
 let myNewObj = {
     "String": "5",
@@ -23,12 +25,12 @@ let myNewObj = {
     "Null": null 
 };
 
-//task5
+                                        //task5
 
 let isAdult = confirm("Are you an adult?");
 alert( isAdult );
 
-//task6
+                                        //task6
 
 const myFirstname = "Serhiy";
 const myLastname = "Pop";
@@ -52,7 +54,7 @@ let playerType;
 console.log(typeof recordType);
 console.log(typeof playerType);
 
-//task7
+                                        //task7
 
 let name; 
 prompt("Dear ", "User");
@@ -61,7 +63,7 @@ prompt("Your password is ", "qwerty");
 name = "Dear User, your email is usermail@gmail.com, your password is qwerty";
 alert(name);
 
-//task8
+                                        //task8
 
 let secondsInOneHour = 60 * 60;
 alert(secondsInOneHour);
@@ -70,4 +72,99 @@ let secondsIn24Hours = 60* 60 * 24;
 alert(secondsIn24Hours);
 
 let secondsInOneMonth = 60 * 60 * 24 * 30;
-alert(secondsInOneMonth);
+alert(secondsInOneMonth); */
+
+
+                                // Second Hometask:
+                                
+                                 //task1
+let x = 1;
+let y = 2;
+let res1 = String(x) + (y)
+console.log(res1);
+console.log(typeof res1);
+
+let res2 = Boolean(x) + String(y)
+console.log(res2);
+console.log(typeof res2);
+
+let res3 = Boolean(x < y)
+console.log(res3);
+console.log(typeof res3);
+
+let res4 = isNaN(x) / isNaN(y);
+console.log(res4);
+console.log(typeof res4);
+
+                                 //task2
+let someNumber;
+someNumber = prompt("Check if number is: a) Even positive; b) Multiple of seven", "Enter number here");
+console.log(someNumber % 2 < 1);
+console.log(someNumber % 7 === 0);
+
+                                 //task3
+let myArr = [1, "space", true, null];
+alert(myArr.length);
+
+let enterVal = prompt("Enter your number");
+myArr.push(enterVal);
+console.log(myArr[4]);
+
+myArr.pop([4]);
+console.log(myArr);
+
+                                 //task4
+let cities = ["Rome", "Lviv", "Warsaw"];
+let citiesStr = cities.toString();
+console.log(citiesStr);
+
+                                 //task5
+let enteredAge = prompt("What is Your Age?", "Enter your age here")
+if (enteredAge < 18){
+    alert("You are too young");
+} else {
+    alert("You have reached the age of majority");
+}
+
+                                 //task6
+
+let side1 = prompt("Enter side1: ", "Enter value here");
+let side2 = prompt("Enter side2: ", "Enter value here");
+let side3 = prompt("Enter side3: ", "Enter value here");
+let triangleSemPer = (side1 + side2 + side3) / 2;
+let triangleArea = Math.sqrt(triangleSemPer * (triangleSemPer - side1) * (triangleSemPer - side2) * (triangleSemPer - side3));
+triangleArea = triangleArea.toFixed(3)
+if (side1 > 0 && side2 > 0 && side3 > 0) {
+    console.log(triangleArea);
+} else {
+    console.log("Incorrect data");
+};
+if ((side3 ** 2) === (side1 ** 2) + (side2 ** 2)) {
+    console.log("This is the right triangle");
+} else {
+    console.log("This isn't the right triangle");
+}
+
+                                    //task7
+//first method
+let currHour = new Date().getHours();
+let greetMessage;
+if (currHour >= 23 && currHour < 5) {
+    greetMessage = "Good night";
+} else if (currHour >= 5 && currHour < 11) {
+    greetMessage = "Good morning";
+} else if (currHour >= 11 && currHour < 17) {
+    greetMessage = "Good day";
+} else if (currHour >= 17 && currHour < 23) {
+    greetMessage = "Good evening";
+}
+alert(greetMessage)
+
+//second method
+let currHour = new Date().getHours();
+let greetMessage;
+(currHour >= 23 && currHour < 5) ? greetMessage = "Good night" :
+(currHour >= 5 && currHour < 11) ? greetMessage = "Good morning" :
+(currHour >= 11 && currHour < 17) ? greetMessage = "Good day" :
+greetMessage = "Good evening";
+alert(greetMessage);
