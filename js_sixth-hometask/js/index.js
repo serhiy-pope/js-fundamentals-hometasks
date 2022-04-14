131b428b1910a41bd8ab6bd41458ec36874b2a9d
Serhiy Pop <sergey.pope@gmail.com>
2022-04-14 17:29:29 +0300

    added 6 task from 6 HT

--- js_sixth-hometask/js/index.js
+++ js_sixth-hometask/js/index.js
@@ -8,23 +8,29 @@ document.body.children[0].innerHTML = "Last1111";
 alert(document.body.children[0].innerHTML);
 
 //-------------------------------------Task 2-----------------------------
-let img = (document.body.getElementsByClassName("image").src = "/js_sixth-hometask/img/cat.jpeg");
+let img = (document.body.getElementsByClassName("image").src =
+  "/js_sixth-hometask/img/cat.jpeg");
 alert(img);
 
 //-------------------------------------Task 3-----------------------------
 let pArr = [];
 let p = document.querySelectorAll("#text > p");
 for (let i = 0; i < p.length; i++) {
-    pArr.push(p[i])
-    console.log("Selector text " + [i] + ": " + pArr[i].innerHTML);
+  pArr.push(p[i]);
+  console.log("Selector text " + [i] + ": " + pArr[i].innerHTML);
 }
 
 //-------------------------------------Task 4-----------------------------
 //fistMethod
 console.log(document.getElementById("list").firstElementChild.innerHTML);
 console.log(document.getElementById("list").lastElementChild.innerHTML);
-console.log(document.getElementById("list").firstElementChild.nextElementSibling.innerHTML);
-console.log(document.getElementById("list").lastElementChild.previousElementSibling.innerHTML);
+console.log(
+  document.getElementById("list").firstElementChild.nextElementSibling.innerHTML
+);
+console.log(
+  document.getElementById("list").lastElementChild.previousElementSibling
+    .innerHTML
+);
 console.log(document.getElementById("list").children[2].innerHTML);
 
 //secondMethod
@@ -48,6 +54,18 @@ myDiv.children[3].style.fontStyle = "italic";
 document.body.children[9].style.display = "none";
 
 //-------------------------------------Task 6-----------------------------
+let input1 = prompt();
+document.getElementById("input1").value = input1;
 
+let input2 = prompt();
+document.getElementById("input2").value = input2;
+
+function changePlaces(a, b) {
+  a = document.getElementById("input2").value = input1;
+  input1 = a;
+  b = document.getElementById("input1").value = input2;
+  input2 = b;
+}
+changePlaces(input1, input2);
 
 //-------------------------------------Task 7-----------------------------
